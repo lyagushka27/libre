e82e418702b756843ece13268a3f810fbd1f6c3f
lyagushka27 <116496687+lyagushka27@users.noreply.github.com>
2023-06-07 18:47:49 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,55 @@
 /**
- * Класс книги, имеющий только геттеры на свои переменные
+ * Книга в библеотеке
  */
 public class Book {
+    /**
+     * @see Book#name   Название книги
+     * @see Book#author Автор книги
+     * @see Book#owner  Пользователь у которого в данных момент находится эта книга, null если книг в библеотеке
+     */
     private final String name;
     private final String author;
-    
+    private Reader owner;
+
+    /**
+     * Конструктор новой книги
+     * @param name      Имя книги
+     * @param author    Автор книги
+     */
     public Book(String name, String author){
         this.name = name;
         this.author = author;
     }
 
+    /**
+     * Геттер
+     * @return Автор
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Геттер
+     * @return Название книги
+     */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Геттер
+     * @return Владелец книги (null если книга в библеотеке)
+     */
+    public Reader getOwner() {
+        return owner;
+    }
+
+    /**
+     * Сеттер
+     * @param r Читатель, которому отдана книга
+     */
+    public void setOwner(Reader r){
+        this.owner = r;
     }
 }
